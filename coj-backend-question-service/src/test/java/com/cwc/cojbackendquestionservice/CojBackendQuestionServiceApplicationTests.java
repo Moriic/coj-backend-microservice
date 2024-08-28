65aade4546c55d3090d59e739a3d8cde779a0aca
1-cwc <2021694476@qq.com>
2024-08-29 00:13:07 +0800

    fix: address

---
+++ coj-backend-question-service/src/test/java/com/cwc/cojbackendquestionservice/CojBackendQuestionServiceApplicationTests.java
@@ -0,0 +1,7 @@
+package com.cwc.cojbackendquestionservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CojBackendQuestionServiceApplicationTests {
+}
--- coj-backend-user-service/src/test/java/com/cwc/cojbackenduserservice/CojBackendUserServiceApplicationTests.java
+++ coj-backend-user-service/src/test/java/com/cwc/cojbackenduserservice/CojBackendUserServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class cojBackendUserServiceApplicationTests {
+class CojBackendUserServiceApplicationTests {
 
     @Test
     void contextLoads() {
