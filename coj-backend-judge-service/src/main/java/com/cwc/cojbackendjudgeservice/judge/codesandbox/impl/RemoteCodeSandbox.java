f848aab46fbfed5e282e2cc8ae31ac0ed0d111b8
Morii <2021694476@qq.com>
2024-08-30 15:57:01 +0800

    Update RemoteCodeSandbox.java

--- coj-backend-judge-service/src/main/java/com/cwc/cojbackendjudgeservice/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ coj-backend-judge-service/src/main/java/com/cwc/cojbackendjudgeservice/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -2,16 +2,22 @@ package com.cwc.cojbackendjudgeservice.judge.codesandbox.impl;
 
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+
 import com.cwc.cojbackendcommon.common.ErrorCode;
 import com.cwc.cojbackendcommon.exception.BusinessException;
 import com.cwc.cojbackendjudgeservice.judge.codesandbox.CodeSandbox;
 import com.cwc.cojbackendmodel.model.codesandbox.ExecuteCodeRequest;
 import com.cwc.cojbackendmodel.model.codesandbox.ExecuteCodeResponse;
+import com.cwc.cojbackendmodel.model.codesandbox.JudgeInfo;
+import com.cwc.cojbackendmodel.model.enums.JudgeInfoMessageEnum;
+
 import org.apache.commons.lang3.StringUtils;
 
 /**
  * 远程代码沙箱（实际调用接口的沙箱）
  */
+@Slf4j
 public class RemoteCodeSandbox implements CodeSandbox {
 
     // 定义鉴权请求头和密钥
@@ -19,19 +25,31 @@ public class RemoteCodeSandbox implements CodeSandbox {
 
     private static final String AUTH_REQUEST_SECRET = "secretKey";
 
-
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         System.out.println("远程代码沙箱");
-        String url = "http://47.76.180.81:8090/executeCode";
+        String url = "http://172.28.201.242:8090/executeCode";
         String json = JSONUtil.toJsonStr(executeCodeRequest);
-        String responseStr = HttpUtil.createPost(url)
+        String responseStr;
+        try {
+            responseStr = HttpUtil.createPost(url)
                 .header(AUTH_REQUEST_HEADER, AUTH_REQUEST_SECRET)
                 .body(json)
                 .execute()
                 .body();
+        } catch (Exception e) {
+            log.error("connect remoteSandbox error, message = " + e.getMessage());
+            ExecuteCodeResponse executeCodeResponse = new ExecuteCodeResponse();
+            JudgeInfo judgeInfo = new JudgeInfo();
+            judgeInfo.setMessage(JudgeInfoMessageEnum.SYSTEM_ERROR.getValue());
+            executeCodeResponse.setMessage(JudgeInfoMessageEnum.SYSTEM_ERROR.getValue());
+            executeCodeResponse.setStatus(2);
+            executeCodeResponse.setJudgeInfo(judgeInfo);
+            return executeCodeResponse;
+        }
         if (StringUtils.isBlank(responseStr)) {
-            throw new BusinessException(ErrorCode.API_REQUEST_ERROR, "executeCode remoteSandbox error, message = " + responseStr);
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR,
+                "executeCode remoteSandbox error, message = " + responseStr);
         }
         return JSONUtil.toBean(responseStr, ExecuteCodeResponse.class);
     }
